01e6f5dab4432829d14ccd62f24bcaebc8ef2fd6
Fernando Restelli <fernandomrestelli@gmail.com>
2020-07-06 17:51:05 -0300

    Create On the the form.php

---
+++ On the the form.php
@@ -0,0 +1,9 @@
+// On the the form set hidden imputs that would save the data.:
+<div type="hidden" class="col-12 mb-3">
+         <input type="hidden" name="ContactFuente" id="ContactFuente" value="<?php echo do_shortcode("[utm_source]"); ?>" />
+         </div>
+         <div type="hidden" class="col-12 mb-3">
+             
+         <input type="hidden" name="ContactMedio" id="ContactMedio" value="<?php echo do_shortcode("[utm_medium]"); ?>" />
+         </div>
+    
